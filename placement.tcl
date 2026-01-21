2c5f3e3dbff750a48259f3b8a88d6d0e7dfb4955
Auslin Feni <auslinfeni@gmail.com>
2026-01-22 00:23:07 +0530

    placement.tcl

--- placement.tcl
+++ placement.tcl
@@ -9,7 +9,7 @@ create_scenario -name func::nom -mode func -corner nom
 current_mode func
 current_scenario func::nom
 
-source ./../CONSTRAINTS/full_adder.sdc
+source <constraints>
 
 set_parasitic_parameters -late_spec $parasitic1 -early_spec $parasitic2
 set_app_options -name place.coarse.continue_on_missing_scandef -value true
