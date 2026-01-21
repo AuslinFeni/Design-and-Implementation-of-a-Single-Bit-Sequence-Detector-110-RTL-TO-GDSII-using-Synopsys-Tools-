76113ddc5db291db6018df50bd61536d22bf2abe
Auslin Feni <auslinfeni@gmail.com>
2026-01-21 23:19:02 +0530

    Create run_dc.tcl
    Logic Synthesis using DC Shell is a crucial step in the digital design flow, where high-level RTL code (written in Verilog or VHDL) is converted into a gate-level netlist using Synopsys Design Compiler (DC Shell). This netlist comprises logic gates and interconnections that match the target technology library.
    The DC Shell interface provides a command-line environment to perform synthesis tasks efficiently. Key steps include analyzing the RTL code, elaborating the design, setting design constraints, and performing optimization for area, timing, and power. Commands like analyze, elaborate, compile, and write are essential in this process. The resulting netlist is further used for physical design and timing analysis.

---
+++ run_dc.tcl
@@ -0,0 +1,28 @@
+source -echo -verbose ./rm_setup/dc_setup.tcl
+set RTL_SOURCE_FILES ./../rtl/seq_detector_110_rtl.v
+
+define_design_lib WORK -path ./WORK
+
+#set_dont_use [get_lib_cells */FADD*]
+#set_dont_use [get_lib_cells */HADD*]
+#set_dont_use [get_lib_cells */AO*]
+#set_dont_use [get_lib_cells */OA*]
+#set_dont_use [get_lib_cells */NAND*]
+#set_dont_use [get_lib_cells */XOR*]
+#set_dont_use [get_lib_cells */NOR*]
+#set_dont_use [get_lib_cells */XNOR*]
+#set_dont_use [get_lib_cells */MUX*]
+
+analyze -format verilog ${RTL_SOURCE_FILES}
+set DESIGN_NAME “seq_detector_110”
+elaborate ${DESIGN_NAME}
+current_design ${DESIGN_NAME}
+
+#read_sdc ./../CONSTRAINTS/full_adder.sdc
+
+compile
+
+#compile_ultra
+#report_timing
+write -format verilog -hierarchy -output ${RESULTS_DIR}/seq_detector_110.mapped.v
+
